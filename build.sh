9a516b84b4c7cf71daf1a826b0726a3ce7b6d471
sujan bose <112420725+sujanboseb@users.noreply.github.com>
2025-04-12 00:36:30 +0530

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Create models directory if it doesn't exist
+mkdir -p models
+
+# If you're including your model files in your repository,
+# you don't need to download them, they'll already be there
+
+# Install Python dependencies
+pip install -r requirements.txt
